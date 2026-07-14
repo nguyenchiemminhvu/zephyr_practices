4de3cb482496898a8608dfcf7eaf5fa2d9499a8c
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-07-14 12:52:19 +0900

    refactor

---
+++ test_features/kernel_data_ring_buffer/src/main.c
@@ -0,0 +1,33 @@
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+
+RING_BUF_DECLARE(global_ringbuf, 255);
+
+int main(void)
+{
+    uint8_t local_buffer_mem[255];
+    struct ring_buf local_ringbuf;
+    ring_buf_init(&local_ringbuf, sizeof(local_buffer_mem), local_buffer_mem);
+
+    uint8_t test_data[] = {1, 2, 3, 4, 5};
+
+    for (int i = 0; i < sizeof(test_data); i++) {
+        ring_buf_put(&global_ringbuf, test_data[i]);
+        ring_buf_put(&local_ringbuf, test_data[i]);
+    }
+
+    printk("Global ring buffer contents:\n");
+    uint8_t data;
+    while (ring_buf_get(&global_ringbuf, &data) == 0)
+    {
+        printk("%d ", data);
+    }
+
+    printk("\nLocal ring buffer contents:\n");
+    while (ring_buf_get(&local_ringbuf, &data) == 0)
+    {
+        printk("%d ", data);
+    }
+
+    return 0;
+}
